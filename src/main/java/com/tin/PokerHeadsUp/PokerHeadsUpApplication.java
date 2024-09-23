24697b0a80c82f44829ffa55bf245bc06b9bd5ed
Tin Nguyen <tinnguyen@Tins-MacBook-Pro.local>
2024-09-23 02:31:22 -0400

    First Commit

---
+++ src/main/java/com/tin/PokerHeadsUp/PokerHeadsUpApplication.java
@@ -0,0 +1,13 @@
+package com.tin.PokerHeadsUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerHeadsUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokerHeadsUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tin/PokerHeadsUp/PokerHeadsUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tin.PokerHeadsUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerHeadsUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
