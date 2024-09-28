4195deaae28adbdef770c1f70c8efd1024b487f4
tin-v-nguyen <tinnguyen2002@gmail.com>
2024-09-28 18:04:56 -0400

    Fixed dependency error, enabled Spring Security, added JWT dependencies

--- src/main/java/com/tin/PokerHeadsUp/model/TwoFactorAuth.java
+++ src/main/java/com/tin/PokerHeadsUp/model/TwoFactorAuth.java
@@ -4,6 +4,8 @@ import com.tin.PokerHeadsUp.domain.VerificationType;
 import lombok.Data;
 
 // will be used as an embedded class in User entity so no @Entity
+
+// Data will provide getters setters, equals, hashcode, and toString
 @Data
 public class TwoFactorAuth {
     private boolean isEnabled = false;
